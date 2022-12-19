9c70c18ab9d352c7be0983b28d55298198554a04
Joel Kaneshiro <joel8800@gmail.com>
2022-12-19 00:11:39 -0800

    Start of Day19

---
+++ Day19/Blueprint.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.
+//Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.
+namespace Day19
+{
+    public class Blueprint
+    {
+        public int ID { get; set; }
+        public int OreBots { get; set; }
+        public int OreBotCost { get; set; }
+        public int ClayBots { get; set; }
+        public int ClayBotCost { get; set; }
+        public int ObsBots { get; set; }
+        public int ObsBotCostOre { get; set; }
+        public int ObsBotCostClay { get; set; }
+        public int GeoBots { get; set; }
+        public int GeoBotCostOre { get; set; }
+        public int GeoBotCostObs { get; set; }
+
+        public int Ore { get; set; }
+        public int Clay { get; set; }
+        public int Obsidian { get; set; }
+        public int Geode { get; set; }
+
+        public Blueprint(int iD, int oreBotCost, int clayBotCost, int obsBotCostOre, int obsBotCostClay, int geoBotCostOre, int geoBotCostObs)
+        {
+            ID = iD;
+            OreBotCost = oreBotCost;
+            ClayBotCost = clayBotCost;
+            ObsBotCostOre = obsBotCostOre;
+            ObsBotCostClay= obsBotCostClay;
+            GeoBotCostOre = geoBotCostOre;
+            GeoBotCostObs = geoBotCostObs;
+
+            OreBots = 1;
+            ClayBots = 0;
+            ObsBots = 0;
+            GeoBots = 0;
+
+            Ore = 0;
+            Clay = 0;
+            Obsidian = 0;
+            Geode = 0;
+        }
+
+        public override string ToString()
+        {
+            //return $"{ID},{OreBotCost},{ClayBotCost},{ObsBotCostOre},{ObsBotCostClay},{GeoBotCostOre},{GeoBotCostObs}";
+            return $"{ID}: Ore({OreBots}):{Ore}  Cla({ClayBots}):{Clay}  Obs({ObsBots}):{Obsidian}  Geo({GeoBots}):{Geode}";
+        }
+
+        public void NextMinute()
+        {
+            bool orderOreBot = false;
+            bool orderClayBot = false;
+            bool orderObsBot = false;
+            bool orderGeoBot = false;
+
+            // build first
+            if (Ore >= OreBotCost)
+            {
+                Ore -= OreBotCost;
+                orderOreBot = true;
+            }
+
+            if (Ore >= ClayBotCost)
+            {
+                Ore -= ClayBotCost;
+                orderClayBot = true;
+            }
+            
+            if (Ore >= ClayBotCost)
+            {
+                Ore -= ClayBotCost;
+                orderClayBot = true;
+            }
+
+            if (Ore >= ObsBotCostOre && Clay >= ObsBotCostClay)
+            {
+                Ore -= ObsBotCostOre;
+                Clay -= ObsBotCostClay;
+                orderObsBot = true;
+            }
+
+            if (Ore >= GeoBotCostOre && Obsidian >= GeoBotCostObs)
+            {
+                Ore -= GeoBotCostOre;
+                Obsidian -= GeoBotCostObs;
+                orderGeoBot = true;
+            }
+
+
+            Ore += OreBots;
+            Clay += ClayBots;
+            Obsidian += ObsBots;
+            Geode += GeoBots;
+
+            if (orderOreBot)
+                OreBots++;
+            if (orderClayBot)
+                ClayBots++;
+            if (orderObsBot)
+                ObsBots++;
+            if (orderGeoBot)
+                GeoBots++;
+
+        }
+
+        public int GetQualityLevel()
+        {
+            return ID * Geode;
+        }
+    }
+}
--- Day19/Program.cs
+++ Day19/Program.cs
@@ -1,3 +1,48 @@
 ﻿using AoCUtils;
+using Day19;
+using System.Text.RegularExpressions;
 
-Console.WriteLine("Day19: ");
+Console.WriteLine("Day19: Not Enough Minerals");
+
+string[] input = FileUtil.ReadFileByLine("inputSamp.txt");
+
+Regex re = new(@"\d+");
+
+//Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.
+//Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.
+
+List<Blueprint> bots = new();
+
+foreach (string line in input)
+{
+    MatchCollection mc = Regex.Matches(line, @"\d+");
+    int id = Convert.ToInt32(mc[0].Value);
+    int oreCost = Convert.ToInt32(mc[1].Value);
+    int clayCost = Convert.ToInt32(mc[2].Value);
+    int obsCostOre = Convert.ToInt32(mc[3].Value);
+    int obsCostCly = Convert.ToInt32(mc[4].Value);
+    int geoCostOre = Convert.ToInt32(mc[5].Value);
+    int geoCostObs = Convert.ToInt32(mc[6].Value);
+
+
+    Blueprint bp = new(id, oreCost, clayCost, obsCostOre, obsCostCly, geoCostOre, geoCostObs);
+    bots.Add(bp);
+}
+
+for (int i = 0; i < 24; i++)
+{
+    foreach (var bot in bots)
+    {
+        bot.NextMinute();
+        Console.WriteLine($"{i + 1}: {bot}");
+    }
+    Console.WriteLine();
+}
+
+foreach (var bot in bots)
+{
+    Console.WriteLine($"{bot.ID}: quality:{bot.GetQualityLevel()}");
+}
+
+Console.WriteLine($"Part1: ");
+
