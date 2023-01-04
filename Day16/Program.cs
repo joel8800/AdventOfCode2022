6e06f43f53e3e4da699479e6d7c400bc363df7da
Joel Kaneshiro <joel8800@gmail.com>
2023-01-03 23:19:10 -0800

    Completed Day16

--- Day16/Program.cs
+++ Day16/Program.cs
@@ -1,110 +1,124 @@
 ﻿using AoCUtils;
-using Day16;
-using System.Text;
+using System.Text.RegularExpressions;
+
+// Solution converted from hyper-neutrino's python code. Huge learning experience for me.
 
 Console.WriteLine("Day16: Proboscidea Volcanium");
 
-string[] input = FileUtil.ReadFileByLine("inputSamp.txt");
+string[] input = FileUtil.ReadFileByLine("input.txt");      // Part1: 2119  Part2: 2615
 
-ValveSystem tunnels = new();
-
+// parse input and store valve and tunnel information in dictionaries
+// keyed by valve name - valves returns flow rate
+// keyed by valve name - tunnels returns list of adjacent valves
+Dictionary<string, int> valves = new();
+Dictionary<string, List<string>> tunnels = new();
 foreach (string line in input)
 {
-    string[] sides = line.Split(';', StringSplitOptions.TrimEntries);
-    string[] valveInfo = sides[0].Split(' ');
-    string[] rateInfo = valveInfo[4].Split("=");
-    string[] adjacents = sides[1].Split("valve");
-    
-    ValveRoom v = new(valveInfo[1], rateInfo[1]);
-    v.AddAdjacents(adjacents[1]);
+    MatchCollection reValves = Regex.Matches(line, @"[A-Z][A-Z]");
+    Match reFlow = Regex.Match(line, @"\d+");
 
-    tunnels.Valves.Add(v);
-    Console.WriteLine(v);
+    List<string> adjacents = new();
+    for (int i = 1; i < reValves.Count; i++)
+        adjacents.Add(reValves[i].Value);
+
+    valves.Add(reValves[0].Value, int.Parse(reFlow.Value));
+    tunnels.Add(reValves[0].Value, adjacents);
 }
 
-tunnels.ConnectTunnels();
+// compress graph to only include valves with flow rate > 0
+// record distances between these valves and AA
+List<string> nonEmpty = new();
+Dictionary<string, Dictionary<string, int>> distances = new();
+foreach (var v in valves)
+{
+    if (v.Key != "AA" && valves[v.Key] == 0)
+        continue;
 
-int released = 0;
+    if (v.Key != "AA")
+         nonEmpty.Add(v.Key);
 
-int step = 0;
-ValveRoom us = tunnels.Valves[0];
+    List<string> visited = new() { v.Key };
+    Dictionary<string, int> aa = new() { { v.Key, 0 } };
+    distances.Add(v.Key, aa);
 
-//while (step < 30)
-//{
-//    released += GetReleasedPressure(tunnels);
-    
-//    // if we're here check if we can open valve
-//    if (us.Rate > 0 && us.OnOff == false)
-//    {
-//        us.OnOff = true;
-//        step++;
-//        continue;
-//    }
-//    else
-//    {
-//        // look for best path 
-//        List<int> score = new();
-//        for (int i = 0; i < us.AdjNodes.Count; i++)
-//        {
-//            score.Add(GetPathCost(us.AdjNodes[i], 1));
-//        }
-//        for (int i = 0; i < us.AdjNodes.Count; i++)
-//        {
-//            Console.WriteLine($"score[{i}]: {score[i]}");
-//        }
-//    }
+    Queue<(int d, string p)> q = new();
+    q.Enqueue((0, v.Key));
 
-//    step++;
-//}
+    while (q.Count > 0)
+    {
+        (int distance, string position) = q.Dequeue();
+        
+        foreach (var neighbor in tunnels[position])
+        {
+            if (visited.Contains(neighbor))
+                continue;
 
+            visited.Add(neighbor);
 
+            if (valves[neighbor] > 0)
+                distances[v.Key].Add(neighbor, distance + 1);
 
+            q.Enqueue((distance + 1, neighbor));
+        }
+    }
 
+    distances[v.Key].Remove(v.Key);
+}
 
-Console.WriteLine($"Part1: {released}");
+// create a mapping of valves to bits in bit field
+Dictionary<string, int> indices = new();
+for (int i = 0; i < nonEmpty.Count; i++)
+    indices[nonEmpty[i]] = i;
 
-
+// create a cache to save compute time
+Dictionary<(int time, string valve, int mask), int> cache= new();
 
 
+int answerPt1 = DFS(30, "AA", 0);
+Console.WriteLine($"Part1: {answerPt1}");
 
+// set up for part2
+cache.Clear();
+int splitBits = (1 << nonEmpty.Count) - 1;
+int answerPt2 = 0;
 
+for (int i = 0; i < (splitBits + 1) / 2; i++)
+    answerPt2 = Math.Max(answerPt2, DFS(26, "AA", i) + DFS(26, "AA", (splitBits ^ i)));
+
+Console.WriteLine($"Part2: {answerPt2}");
 
 
 //=============================================================================
 
-int GetPathCost(ValveRoom adj, int level)
+
+// Depth First Search
+// time = remaining time
+// valve = position we're searching from
+// vBitField = valve bit field of valves that are already closed
+int DFS(int time, string valve, int vBitField)
 {
-    if (adj.Rate > 0 && adj.OnOff == false)
+    // if cache has this combination, return the result
+    if (cache.ContainsKey((time, valve, vBitField)))
+        return cache[(time, valve, vBitField)];
+
+    // search for the path that results in the most remaining time
+    int maxVal = 0;
+    foreach (var neighbor in distances[valve])
     {
-        return adj.Rate * level;
+        int bit = 1 << indices[neighbor.Key];
+        
+        if ((vBitField & bit) != 0)
+            continue;
+
+        int remTime = time - distances[valve][neighbor.Key] - 1;
+        if (remTime <= 0)
+            continue;
+        maxVal = Math.Max(maxVal, DFS(remTime, neighbor.Key, vBitField | bit) + valves[neighbor.Key] * remTime);
     }
 
-    List<int> score = new();
-    for (int i = 0; i < adj.AdjNodes.Count; i++)
-    {
-        score.Add(GetPathCost(adj.AdjNodes[i], level + 1));
-    }
-    return score.Max();
+    // cache this combination and value in case we see it again
+    cache[(time, valve, vBitField)] = maxVal;
+
+    return maxVal;
 }
-
-
-
-int GetReleasedPressure(ValveSystem vs)
-{
-    int released = 0;
-    StringBuilder sb = new();
-    sb.Append("opened: ");
-
-    foreach (ValveRoom v in vs.Valves)
-    {
-        if (v.OnOff == true)
-            released += v.Rate;
-        sb.Append($"{v.ID} ");
-    }
-    sb.Append($": {released}");
-    Console.WriteLine(sb.ToString());
-
-    return released;
-}
-
 
--- Day25/Program.cs
+++ Day25/Program.cs
@@ -1,3 +1,64 @@
 ﻿using AoCUtils;
 
 Console.WriteLine("Day25: Full of Hot Air");
+
+string[] input = FileUtil.ReadFileByLine("input.txt");  // Part1: 2--2-0=--0--100-=210
+
+long total = 0;
+foreach (string sNum in input)
+{
+    long decNum = Snafu2Decimal(sNum);
+
+    //// test
+    //string snafu = Decimal2Snafu(decNum);
+    //Console.WriteLine($"decNum: {decNum}  snafuNum: {snafu}");
+
+    total += decNum;
+}
+
+string answerPt1 = Decimal2Snafu(total);
+
+Console.WriteLine($"Part1: {answerPt1}");
+
+//=============================================================================
+
+long Snafu2Decimal(string snafuNum)
+{
+    Dictionary<char, int> s2d = new()
+    { { '2', 2 }, { '1', 1 }, { '0', 0 }, { '-', -1 }, { '=', -2 } };
+
+    char[] revSn = snafuNum.Reverse().ToArray();
+
+    long total = 0;
+    for (int i = 0; i < revSn.Length; i++)
+    {
+        total += s2d[revSn[i]] * Convert.ToInt64(Math.Pow(5, i));
+    }
+
+    return total;
+}
+
+string Decimal2Snafu(long decNum)
+{
+    Dictionary<int, char> d2s = new()
+    { { 2, '2' }, { 1, '1' }, { 0, '0' }, { -1, '-' }, { -2, '=' } };
+
+    List<char> snafuList = new();
+
+    while (decNum > 0)
+    {
+        int digit = Convert.ToInt32(((decNum + 2) % 5) - 2);
+        snafuList.Add(d2s[digit]);
+        
+        decNum -= digit;
+        decNum /= 5;
+    }
+
+    snafuList.Reverse();
+
+    string snafu = string.Empty;
+    foreach (char c in snafuList)
+        snafu += c;
+
+    return snafu;
+}
